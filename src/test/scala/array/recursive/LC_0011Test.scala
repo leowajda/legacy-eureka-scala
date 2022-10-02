6346905821cb3b181276d48dbac6ccd2eb9ff324
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-02 21:04:24 +0200

    :white_check_mark: Add test for recursive 'Container With Most Water'

---
+++ src/test/scala/array/recursive/LC_0011Test.scala
@@ -0,0 +1,33 @@
+package com.eureka
+package array.recursive
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.*
+import array.recursive.LC_0001
+
+import org.scalatest.Inspectors.forAll
+import org.scalatest.Outcome
+
+class LC_0011Test extends AnyFunSuite with Matchers:
+
+  trait Solution:
+    val solution: LC_0011 = LC_0011()
+
+
+  private def parameters = List(
+
+    (Array(1,8,6,2,5,4,8,3,7), 49),
+    (Array(1,1), 1)
+
+  )
+
+  forAll(parameters) { param =>
+    test(s"maxArea => $param") {
+      new Solution:
+
+        val (height, expected) = param
+        val result = solution.maxArea(height)
+
+        result shouldBe expected
+    }
+  }
