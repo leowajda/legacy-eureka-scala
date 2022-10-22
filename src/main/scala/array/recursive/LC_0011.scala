a5beab78bf37f8499137d0c42b23568e5eec2d73
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-22 06:21:57 +0200

    :zap: Use implicits to improve code readability

--- src/main/scala/array/recursive/LC_0011.scala
+++ src/main/scala/array/recursive/LC_0011.scala
@@ -2,20 +2,22 @@ package com.eureka
 package array.recursive
 
 import scala.annotation.tailrec
+import scala.language.postfixOps
 import scala.math.{max, min}
 
 class LC_0011:
+  def area(left: Int, right: Int)(using height : Array[Int]): Int = (right - left) * min(height(left), height(right))
+  def next(state: (Int, Int))(using height: Array[Int]): (Int, Int) = state match
+    case (left, right) if height(left) <= height(right) => (left + 1, right)
+    case (left, right)                                  => (left, right - 1)
+
   def maxArea(height: Array[Int]): Int =
 
-    def area(left: Int, right: Int): Int = (right - left) * min(height(left), height(right))
-
-    def next = (prev: (Int, Int)) => prev match
-      case (left, right) if height(left) <= height(right) => (left + 1, right)
-      case (left, right)                                  => (left, right - 1)
+    given Array[Int] = height
 
     @tailrec
-    def helper(result: Int, indices: (Int, Int)): Int =
-      val (left, right) = indices
-      if left >= right then result else helper(max(result, area(left, right)), next(indices))
+    def helper(result: Int, state: (Int, Int)): Int =
+      val (left, right) = state
+      if left >= right then result else helper(max(result, area(left, right)), next(state))
 
     helper(0, (0, height.length - 1))
