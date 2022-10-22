077a1131df32e97b5f9415cc80cd22601e5627aa
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-22 06:29:31 +0200

    :white_check_mark: Add test for recursive 'Maximum Depth of Binary Tree'

---
+++ src/test/scala/binary_tree/recursive/LC_0104Test.scala
@@ -0,0 +1,36 @@
+package com.eureka
+package binary_tree.recursive
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.*
+import org.scalatest.Inspectors.forAll
+
+import binary_tree.TreeNode.deserialize
+
+class LC_0104Test extends AnyFunSuite with Matchers:
+
+  trait Solution:
+    val solution: LC_0104 = LC_0104()
+
+/**
+   [3,9,20,null,null,15,7]
+   [1,null,2]
+ */
+  private def parameters = List(
+
+    (deserialize(Some(3), Some(9), None, None, Some(15), Some(7)), 3),
+    (deserialize(Some(1), None, Some(2)), 2)
+
+  )
+
+  forAll(parameters) { param =>
+    test(s"maxDepth => $param") {
+      new Solution:
+
+        val (root, expected) = param
+        val result = solution.maxDepth(root)
+
+        result shouldBe expected
+    }
+
+  }
