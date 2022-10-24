5e1a1b3ee588093884df3382b8d61be763514b89
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-24 06:31:01 +0200

    :white_check_mark: Add test for recursive 'Count Good Nodes in Binary Tree'

---
+++ src/test/scala/binary_tree/recursive/LC_1448Test.scala
@@ -0,0 +1,32 @@
+package com.eureka
+package binary_tree.recursive
+
+import binary_tree.TreeNode.deserialize
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.*
+import org.scalatest.Inspectors.forAll
+
+class LC_1448Test extends AnyFunSuite with Matchers:
+
+  trait Solution:
+    val solution: LC_1448 = LC_1448()
+
+    private def parameters = List(
+
+      (deserialize(Some(3), Some(1), Some(4), Some(3), None, Some(1), Some(5)), 4),
+      (deserialize(Some(3), Some(3), None, Some(4), Some(2)),                   3),
+      (deserialize(Some(1)),                                                    1)
+
+    )
+
+    forAll(parameters) { param =>
+      test(s"goodNodes => $param") {
+        new Solution :
+
+          val (root, expected) = param
+          val result = solution.goodNodes(root)
+
+          result shouldBe expected
+      }
+
+    }
