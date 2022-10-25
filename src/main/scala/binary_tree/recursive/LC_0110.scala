02b2678a8444f86d6bde64722e0ed3f200e40230
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-25 18:27:33 +0200

    :sparkles: Add recursive solution for 'Balanced Binary Tree'

---
+++ src/main/scala/binary_tree/recursive/LC_0110.scala
@@ -0,0 +1,18 @@
+package com.eureka
+package binary_tree.recursive
+
+import binary_tree.{Empty, Node, TreeNode}
+import binary_tree.TreeNode.{deserialize, serialize}
+import scala.math.{abs, cos, max}
+
+class LC_0110:
+  def isBalanced(root: TreeNode): Boolean =
+
+    def helper(root: TreeNode): Int = root match
+      case Node(_, left, right) => (helper(left), helper(right)) match
+        case (-1, _) | (_, -1)        => -1
+        case (a, b) if abs(a - b) > 1 => -1
+        case (a, b)                   => max(a, b) + 1
+      case Empty                => 0
+
+    helper(root) != -1
