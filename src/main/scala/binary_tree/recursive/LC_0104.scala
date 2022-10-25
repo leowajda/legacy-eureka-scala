ea301694b5714afb805bb481f5f0d803214002b3
Leonardo Wajda <dev.leowajda@tuta.io>
2022-10-25 18:27:01 +0200

    :art: Fix spacing

--- src/main/scala/binary_tree/recursive/LC_0104.scala
+++ src/main/scala/binary_tree/recursive/LC_0104.scala
@@ -2,11 +2,11 @@ package com.eureka
 package binary_tree.recursive
 
 import binary_tree.{Empty, Node, TreeNode}
+import math.max
 
 class LC_0104:
-  def maxDepth(root: TreeNode): Int =
-    root match
-      case node: Node => Math.max(maxDepth(node.left), maxDepth(node.right)) + 1
+  def maxDepth(root: TreeNode): Int = root match
+      case node: Node => max(maxDepth(node.left), maxDepth(node.right)) + 1
       case Empty       => 0
 
 
