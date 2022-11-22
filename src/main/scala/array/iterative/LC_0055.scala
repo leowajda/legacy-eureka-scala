cd0ed6379c435326be5bfcac21bdc6e5b3c1388a
Leonardo Wajda <dev.leowajda@tuta.io>
2022-11-22 17:09:08 +0100

    :sparkles: Add iterative solution for 'Jump Game'

---
+++ src/main/scala/array/iterative/LC_0055.scala
@@ -0,0 +1,8 @@
+package com.eureka
+package array.iterative
+
+class LC_0055:
+  def canJump(nums: Array[Int]): Boolean =
+    nums.indices.foldRight(nums.length - 1) {
+      (b, a) => if b + nums(b) >= a then b else a
+    } == 0
