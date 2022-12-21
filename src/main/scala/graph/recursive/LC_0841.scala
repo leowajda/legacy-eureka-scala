3e480c5e25f823a7387ca0aee276d1e6143b29dc
Leonardo Wajda <dev.leowajda@tuta.io>
2022-12-21 11:11:44 +0100

    :sparkles: Add recursive solution for 'Keys and Rooms'

---
+++ src/main/scala/graph/recursive/LC_0841.scala
@@ -0,0 +1,15 @@
+package com.eureka
+package graph.recursive
+
+import scala.annotation.tailrec
+
+class LC_0841:
+  def canVisitAllRooms(rooms: List[List[Int]]): Boolean =
+
+    @tailrec
+    def helper(stack: List[Int], set: Set[Int], seq: IndexedSeq[List[Int]]): Set[Int] = stack match
+      case head :: next => helper(seq(head).filterNot(set.contains) ++: next, set + head, seq)
+      case Nil          => set
+
+    helper(rooms.head, Set(0), rooms.toIndexedSeq).size == rooms.size
+
