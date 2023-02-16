8bacb0447d45aba8815d410683c759d23fda6b1a
Leonardo Wajda <dev.leowajda@tuta.io>
2023-02-16 14:00:21 +0100

    :white_check_mark: Add test for iterative 'Maximum Depth of Binary Tree'

---
+++ src/test/scala/binary_tree/iterative/LC_0104.scala
@@ -0,0 +1,26 @@
+package com.eureka
+package binary_tree.iterative
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.shouldBe
+import org.scalatest.prop.TableDrivenPropertyChecks.{Table, forAll}
+
+import binary_tree.TreeNode.levelOrderDeserialize
+
+class LC_0104Test extends AnyFunSuite:
+
+  trait Solution:
+    val solution: LC_0104 = LC_0104()
+
+  private def parameters = Table(
+    ("root",                                                                "expected"),
+    (levelOrderDeserialize(Some(3), Some(9), None, None, Some(15), Some(7)),         3),
+    (levelOrderDeserialize(Some(1), None, Some(2)),                                  2)
+  )
+
+  forAll(parameters) { (root, expected) =>
+    new Solution:
+
+      val result = solution.maxDepth(root)
+      result shouldBe expected
+  }
