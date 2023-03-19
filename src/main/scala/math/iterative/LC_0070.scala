78688d3b7d6423b2b83ec6f41e6c3c54bbbe3662
Leonardo Wajda <dev.leowajda@tuta.io>
2023-03-19 10:33:44 +0100

    :zap: Use lazy evaluation

--- src/main/scala/math/iterative/LC_0070.scala
+++ src/main/scala/math/iterative/LC_0070.scala
@@ -2,7 +2,7 @@ package com.eureka
 package math.iterative
 
 class LC_0070:
-  def climbStairs(n: Int): Int = n match
-      case 1 | 2 => n
-      case _     => (3 to n).foldLeft((1, 2)) { case ((a, b), _) => (b, a + b) }._2
+  def climbStairs(n: Int): Int =
+    lazy val seq: LazyList[Int] = 1 #:: 2 #:: seq.zip(seq.tail).map { (a, b) => a + b }
+    seq.take(n).last
 
--- src/main/scala/math/iterative/LC_0509.scala
+++ src/main/scala/math/iterative/LC_0509.scala
@@ -2,6 +2,6 @@ package com.eureka
 package math.iterative
 
 class LC_0509:
-  def fib(n: Int): Int = n match
-      case 0 | 1 => n
-      case _     => (2 to n).foldLeft((0, 1)) { case ((a, b), _) => (b, a + b) }._2
+  def fib(n: Int): Int =
+    lazy val fibs: LazyList[Int] = 0 #:: 1 #:: fibs.zip(fibs.tail).map{ (a, b) => a + b }
+    fibs.take(n + 1).last
